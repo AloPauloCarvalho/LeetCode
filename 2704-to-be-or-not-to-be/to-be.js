bcb0aa876e4ca2566a1fa66f74650c00d99b9a58
Paulo Carvalho <alopaulocarvalho@gmail.com>
2024-12-04 17:16:30 -0300

    To Be or Not to Be

---
+++ 2704-to-be-or-not-to-be/to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string|number} val
+ * @return {Object}
+ */
+var expect = function (val) {
+    return {
+        toBe: function (num) {
+            if (num === val) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function (num) {
+            if (num !== val) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};
